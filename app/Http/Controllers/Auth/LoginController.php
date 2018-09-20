339a23d1256ff7d4577483f045c357bdfaf0508a
CDamasse <charles.damasse@gmail.com>
2018-09-20 18:06:00 +0200

    Inscription / login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -24,8 +25,16 @@ class LoginController extends Controller
      * Where to redirect users after login.
      *
      * @var string
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    protected $redirectTo = '/home';
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->isAdmin()){
+            return redirect()->route('admin.index');
+        }else{
+            return redirect()->route('client.index');
+        }
+    }
 
     /**
      * Create a new controller instance.
